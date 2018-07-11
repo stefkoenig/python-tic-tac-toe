a9b6b7cf41d095ce8f281547f1105fd989e6c9b7
Stefanie Koenig <stefanie.koenig@dollarshaveclub.com>
2018-07-11 14:48:44 -0700

    [Tic Tac Toe] Define players edits and start work on printing board

--- tictactoe.py
+++ tictactoe.py
@@ -1,19 +1,40 @@
 def define_players():
-  player1 = input('Player 1, you will begin. Would you like to play as X or Y? ').upper()
-  
-  while player1 != 'X' and player1 != 'Y':
-    player1 = input('Please choose X or Y ').upper()
+  player1 = input('Player 1, you will begin. Would you like to play as X or O? ').upper()
+
+  while player1 != 'X' and player1 != 'O':
+    player1 = input('Please choose X or O ').upper()
   if player1 == 'X':
-    player2 = 'Y'
+    player2 = 'O'
   else:
     player2 = 'X'
+
+  players = dict()
+  players['player1'] = player1
+  players['player2'] = player2
+  players['activePlayer'] = player1
 
   print('player1 you are: ', player1)
   print('player2 you are: ', player2)
 
+  return players
+
+def print_board():
+  board = [{"7": ""},{8: ""},{9: ""}, {4: ""},{5: ""},{6: ""}, {1: ""},{2:""},{3:""}]
+  print("----------------")
+  for i in board:
+    boardValue = list(i.values())
+    print(boardValue[0])
+    # print("|", j, "|", end = " ")
+    print()
+    print("----------------")
+
+
 def setup_game():
   print('Hello! Welcome to Tic Tac Toe')
-  define_players()
+  players = define_players()
+  print_board()
+  activePlayer = players['activePlayer']
+  print(activePlayer)
 
 
 setup_game()
