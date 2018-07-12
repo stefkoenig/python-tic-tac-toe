0b9f9c18db60f4d24c48bf6e661981f4093b258e
Stefanie Koenig <stefanie.koenig@dollarshaveclub.com>
2018-07-11 17:53:19 -0700

    [Tic Tac Toe] Completed Game woot woot

--- tictactoe.py
+++ tictactoe.py
@@ -18,23 +18,74 @@ def define_players():
 
   return players
 
-def print_board():
-  board = [{"7": ""},{8: ""},{9: ""}, {4: ""},{5: ""},{6: ""}, {1: ""},{2:""},{3:""}]
-  print("----------------")
-  for i in board:
-    boardValue = list(i.values())
-    print(boardValue[0])
-    # print("|", j, "|", end = " ")
-    print()
-    print("----------------")
+def print_board(board):
+  print('  ' + board[7] + '  |  ' + board[8] + '  |  ' + board[9] )
+  print('------------------')
+  print('  ' + board[4] + '  |  ' + board[5] + '  |  ' + board[6] )
+  print('------------------')
+  print('  ' + board[1] + '  |  ' + board[2] + '  |  ' + board[3] )
 
+def take_turn(board, players, winner):
+  activePlayer = players['activePlayer']
+  print("It's your turn, ", activePlayer)
+  userInput = int(input('Choose where you want to go '))
+  while board[userInput] != ' ':
+    userInput = int(input('Please choose an empty tile '))
+  else:
+    board[userInput] = activePlayer
+    print_board(board)
+    winner = check_for_winner(board, players, winner)
+    if activePlayer == players['player1']:
+      players['activePlayer'] = players['player2']
+    else:
+      players['activePlayer'] = players['player1']
+
+
+  gameState = dict()
+  gameState['board'] = board
+  gameState['players'] = players
+  gameState['winner'] = winner
+
+  return gameState
+
+def check_for_winner(board, players, winner):
+  activePlayer = players['activePlayer']
+
+  if ((board[1] == activePlayer and board[2] == activePlayer and board[3] == activePlayer) or
+  (board[4] == activePlayer and board[5] == activePlayer and board[6] == activePlayer) or
+  (board[7] == activePlayer and board[8] == activePlayer and board[9] == activePlayer) or 
+  (board[1] == activePlayer and board[5] == activePlayer and board[9] == activePlayer) or
+  (board[7] == activePlayer and board[5] == activePlayer and board[3] == activePlayer) or
+  (board[7] == activePlayer and board[4] == activePlayer and board[1] == activePlayer) or
+  (board[8] == activePlayer and board[5] == activePlayer and board[2] == activePlayer) or
+  (board[9] == activePlayer and board[6] == activePlayer and board[3] == activePlayer)):
+    print(activePlayer + ' you win!')
+    winner = True
+  elif all([board[x] != ' ' for x in range(1,10)]):
+    print('No winner!')
+    winner = 'tie'
+  else:
+    winner = False
+  return winner
 
 def setup_game():
+  winner = False
+  board = [' '] * 10
   print('Hello! Welcome to Tic Tac Toe')
   players = define_players()
-  print_board()
-  activePlayer = players['activePlayer']
-  print(activePlayer)
+  print_board(board)
+  play_game(board, players, winner)
+
+def play_game(board, players, winner):
+  while winner == False:
+   gameState = take_turn(board, players, winner)
+   winner = gameState['winner']
+  else:
+    restartGame = input('Would you like to play again? Y or N? ').upper()
+    if restartGame == 'Y':
+      setup_game()
+    else:
+      print('Thanks for playing!')
 
 
 setup_game()
